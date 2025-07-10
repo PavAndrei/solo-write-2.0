738191d226ed26eb1628b60d3302c1f650d66100
Andrei Pavlov <andrpav9@gmail.com>
2025-07-10 21:32:51 +0400

    fix: fix background and text for the footer while it is dark mode

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { FaCopyright } from 'react-icons/fa';
 
 export const Footer = () => {
   return (
-    <footer className="mt-auto mb-0 bg-gray-100 border-t border-t-gray-400 pt-10">
-      <span className="p-3 w-full border-t border-gray-400 flex items-center justify-center gap-1.5 text-gray-500">
+    <footer className="mt-auto mb-0 bg-gray-100 border-t border-t-gray-400 pt-10 dark:bg-gray-900">
+      <span className="p-3 w-full border-t border-gray-400 flex items-center justify-center gap-1.5 text-gray-500 dark:text-gray-300">
         <FaCopyright />
         <span>All rights reserved</span>
       </span>
