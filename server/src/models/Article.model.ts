9ff80d448a286833bae6e4237c55de0b25c866f7
Andrei Pavlov <andrpav9@gmail.com>
2025-07-18 06:15:17 +0400

    feat: add article schemas to the backend

---
+++ server/src/models/Article.model.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const articleSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    categories: {
+      type: Array,
+      requred: true,
+      default: [],
+    },
+
+    images: {
+      type: Array,
+      default: [],
+    },
+    viewsCount: { type: Number, default: 0 },
+    likesCount: { type: Number, default: 0 },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Article = mongoose.model('Article', articleSchema);
+export default Article;
