420f27991c71e9a5dc8e7c5ae507efdcde7319e7
Andrei Pavlov <andrpav9@gmail.com>
2025-07-26 16:30:55 +0400

    refactor: make articles page work with custom hook useFetch

--- client/src/api/apiArticle.ts
+++ client/src/api/apiArticle.ts
@@ -18,6 +18,38 @@ export const createArticle = async (formData: FormData): Promise<ApiResponse> =>
   }
 };
 
+type getAllArticlesParams = {
+  startIndex?: string;
+  limit?: string;
+  order?: string;
+  userId?: string;
+  category?: string;
+  searchTerm?: string;
+};
+
+export const getAllArticles = async (params?: getAllArticlesParams): Promise => {
+  const startIndex = params?.startIndex ? `startIndex=${params.startIndex}` : '';
+  const limit = params?.limit ? `limit=${params.limit}` : '';
+  const order = params?.order ? `order=${params.order}` : '';
+  const userId = params?.userId ? `userId=${params.userId}` : '';
+  const category = params?.category ? `category=${params.category}` : '';
+  const searchTerm = params?.searchTerm ? `searchTerm=${params.searchTerm}` : '';
+
+  try {
+    const res = await fetch(
+      `http://localhost:5000/api/article${params ? '?' : ''}${startIndex}${limit}${order}${userId}${category}${searchTerm}`
+    );
+
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    return {
+      success: false,
+      message: err instanceof Error ? err.message : 'Unknown error',
+    };
+  }
+};
+
 export const toggleArticleLike = async (id: string): Promise<ApiResponse> => {
   try {
     const response = await fetch(`http://localhost:5000/api/article/${id}/like`, {
@@ -34,16 +66,3 @@ export const toggleArticleLike = async (id: string): Promise<ApiResponse> => {
     };
   }
 };
-
-// export const getAllArticles = async (params): Promise<ApiResponse> => {
-//   try {
-//     const response = await fetch('http://localhost:5000/api/article/', {
-//       method: 'GET',
-//     });
-//   } catch (err) {
-//     return {
-//       success: false,
-//       message: err instanceof Error ? err.message : 'Unknown error',
-//     };
-//   }
-// };
--- client/src/pages/Articles.tsx
+++ client/src/pages/Articles.tsx
@@ -3,21 +3,11 @@ import { AnimationProvider } from '../components/AnimationProvider';
 import { Container } from '../components/Container';
 import { PageTitle } from '../components/PageTitle';
 import { ArticlesList } from '../components/ArticlesList';
-import { useEffect, useState } from 'react';
+import { getAllArticles } from '../api/apiArticle';
+import { useFetch } from '../hooks/useFetch';
 
 export const Articles = () => {
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    const fetchArticles = async () => {
-      const res = await fetch('http://localhost:5000/api/article');
-
-      const data = await res.json();
-      setArticles(data.articles);
-    };
-
-    fetchArticles();
-  }, []);
+  const { data, error, isLoading } = useFetch(getAllArticles);
 
   return (
     <AnimatePresence>
@@ -25,7 +15,7 @@ export const Articles = () => {
         <Container>
           <section className="py-[60px] md:py-[100px] xl:py-[120px]">
             <PageTitle>Articles</PageTitle>
-            {articles && <ArticlesList articles={articles}></ArticlesList>}
+            {data && <ArticlesList articles={data.articles} />}
           </section>
         </Container>
       </AnimationProvider>
